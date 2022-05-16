679f5f51caf2590037c3091088d71c364757c8be
wangxingkang <wang_xingkang@qq.com>
2022-05-16 15:07:45 +0800

    ✨ feat: loader组件优化

--- src/api-loader.tsx
+++ src/api-loader.tsx
@@ -1,25 +1,14 @@
-import React, { useEffect } from 'react';
-import { useExternal, useBoolean } from '@pansy/react-hooks';
+import React from 'react';
+import { useExternal } from '@pansy/react-hooks';
 
 export const ApiLoader: React.FC = ({
   children,
 }) => {
-  const [loaded, loadedAction] = useBoolean(false);
-
   const status = useExternal('//g.alicdn.com/AWSC/AWSC/awsc.js');
-
-  useEffect(
-    () => {
-      loadedAction.set(
-        status === 'ready'
-      )
-    },
-    [status]
-  );
 
   return (
     <>
-      {loaded && children}
+      {(status === 'ready') && children}
     </>
   )
 }
