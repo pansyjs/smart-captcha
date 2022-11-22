a114f010ae4f6f7231865c0e57441e5e03543eef
wangxingkang <wang_xingkang@qq.com>
2022-11-22 13:29:31 +0800

    fix: 修复类型报错

--- src/api-loader.tsx
+++ src/api-loader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useExternal } from '@pansy/react-hooks';
 
-export const ApiLoader: React.FC = ({
+export const ApiLoader: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const status = useExternal('//g.alicdn.com/AWSC/AWSC/awsc.js');
