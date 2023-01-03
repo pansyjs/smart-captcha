c15f65f1b306a34ea963152cad539abe2f3e9629
wangxingkang <wang_xingkang@qq.com>
2023-01-03 11:21:28 +0800

    feat: 支持文案自定义

--- src/smart-captcha.tsx
+++ src/smart-captcha.tsx
@@ -17,6 +17,12 @@ const SmartCaptcha: React.ForwardRefRenderFunction<SmartCaptchaRef, SmartCaptcha
     scene = Option.scene,
     width = 300,
     height = 42,
+    defaultTxt = '点击按钮开始智能验证',
+    successTxt = '验证成功',
+    scaningTxt = '智能检测中',
+    failTxt = '验证失败，请在此点击按钮刷新',
+    language,
+    upLang,
     onSuccess,
     onChange,
     onFailed,
@@ -33,7 +39,7 @@ const SmartCaptcha: React.ForwardRefRenderFunction<SmartCaptchaRef, SmartCaptcha
 
   useEffect(() => {
     if (window.AWSC) {
-      window.AWSC.use('ic', function (state: string, module: any) {
+      window.AWSC.use('ic', (_: string, module: any) => {
         ic.current = module.init({
           // 声明智能验证需要渲染的目标元素ID。
           renderTo: `#${elementId}`,
@@ -42,17 +48,19 @@ const SmartCaptcha: React.ForwardRefRenderFunction<SmartCaptchaRef, SmartCaptcha
           // 智能验证组件的高度。
           height: height,
           // 智能验证组件初始状态文案。
-          default_txt: '点击按钮开始智能验证',
+          default_txt: defaultTxt,
           // 智能验证组件验证通过状态文案。
-          success_txt: '验证成功',
+          success_txt: successTxt,
           // 智能验证组件验证失败（拦截）状态文案。
-          fail_txt: '验证失败，请在此点击按钮刷新',
+          fail_txt: failTxt,
           // 智能验证组件验证中状态文案。
-          scaning_txt: '智能检测中',
+          scaning_txt: scaningTxt,
           // 应用类型标识
           appkey,
           // 使用场景标识
           scene,
+          language,
+          upLang,
           success: function(data: SmartCaptchaResult) {
             const value = {
               sig: data.sig,
--- src/types.ts
+++ src/types.ts
@@ -30,6 +30,41 @@ export interface SmartCaptchaProps {
    * @default 'smart-captcha'
    */
   elementId?: string;
+  /**
+   * 智能验证组件初始状态文案
+   */
+  defaultTxt?: string;
+  /**
+   * 智能验证组件验证通过状态文案
+   */
+  successTxt?: string;
+  /**
+   * 智能验证组件验证失败（拦截）状态文案
+   */
+  failTxt?: string;
+  /**
+   * 验证组件的语言
+   * @default 'cn'
+   */
+  language?: string;
+  /**
+   * 二次验证时，用于自定义文案。
+   */
+  upLang?: Record<string, {
+    LOADING?: string;
+    SLIDE?:  string;
+    SUCCESS?: string;
+    ERROR?: string;
+    FAIL?: string;
+  }>;
+  /**
+   * 测试字段，用于测试验证码的不同状态
+   */
+  test?: string;
+  /**
+   * 智能验证组件验证中状态文案
+   */
+  scaningTxt?: string;
   /** 组件宽度 */
   width?: number;
   /** 组件高度 */
